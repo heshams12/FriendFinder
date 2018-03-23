65e6e731e84763d99bc1799f8c5704bc8d439452
Unknown <Heshams12@hotmail.com>
2018-03-22 20:45:58 -0700

    updated FriendFinder assignment.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
-var friendArray = [
+var Friends = [
     {
-    Name: "Bill Smith",
+    name: "Bill Smith",
     imageLink: "https://pixel.nymag.com/imgs/fashion/daily/2016/02/26/26-waynes-world-garth-stare.w710.h473.jpg",
     scores: [4,
      5,
@@ -16,7 +16,7 @@ var friendArray = [
 },
 
 {
-    Name: "Eric Lee",
+    name: "Eric Lee",
     imageLink: "http://i.dailymail.co.uk/i/pix/2012/12/18/article-0-168CD5FA000005DC-154_634x523.jpg",
     scores: [3,
     2,
@@ -31,7 +31,7 @@ var friendArray = [
 },
 
 {
-    Name: "Kelly Madison",
+    name: "Kelly Madison",
     imageLink: "http://www.kbc.co.ke/wp-content/uploads/2017/04/kim-1.jpg",
     scores: [1,
     1,
@@ -45,7 +45,7 @@ var friendArray = [
      2]
 },
 {
-    Name: "Jessica Sanchez",
+    name: "Jessica Sanchez",
     imageLink: "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2017%2F12%2Fgettyimages-687357612.jpg%3Fw%3D2000&w=700&q=85",
     scores: [4,
     3,
@@ -62,4 +62,4 @@ var friendArray = [
 
 
 
-module.exports = friendArray;
+module.exports = Friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,72 +1,63 @@
-var friends = require("../data/friends");
+var Friends = require("../data/friends.js");
 var path = require("path")
 
 module.exports = function (app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+
 
   app.get("/api/friends", function (req, res) {
-    res.json(friends);
+    res.json(Friends);
   });
 
 
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+
 
   app.post("/api/friends", function (req, res) {
-    //console.log(JSON.stringify(req.body.scores, null, 2));
+
 
     var friendScores = req.body.scores;
 
     var matchName = '';
     var matchImage = '';
-    var totalDifference = 0;
+    var leastSum = 10000;
 
-    for (var i = 0; i < friends.length; i++) {
-      // console.log('friend = ' + JSON.stringify(friends[i]));
+    for (var i = 0; i < Friends.length; i++) {
 
-      // Compute differenes for each question
-      var difference = [];
+
       var sumDiff = 0;
       for (var j = 0; j < friendScores.length; j++) {
-       sumDiff += (Math.abs(friends[i].scores[j] - friendScores[j]));
-
-      } console.log(difference.push(sumDiff));
+        sumDiff += (Math.abs(Friends[i].scores[j]) - (friendScores[j]));
 
 
-      console.log('difference = ' + difference);
+      }
 
-      // If lowest difference, record the friend match
-      if (difference < totalDifference) {
-        totalDifference = difference;
-        matchName = friends[i].Name;
-        matchImage = friends[i].imageLink;
-       
-        console.log('Closest match found = ' + difference);
-        console.log('Friend name = ' + matchName);
-        console.log('Friend image = ' + matchImage);
 
+
+
+
+      if (sumDiff < leastSum) {
+
+
+        matchName = Friends[i].name;
+        matchImage = Friends[i].imageLink;
+        leastSum = sumDiff;
         
+        console.log(matchName);
+
+
+
       }
     }
 
 
 
 
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
-    friends.push(req.body) //this should be done last!
+    Friends.push(req.body)
 
+    res.json({
+      name: matchName,
+      image: matchImage
+    })
   });
 
 
